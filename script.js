24c4f38b535b5fa55bcaffdc1e6e331f8c5aa11d
ctyhuofc <98438791+ctyhuofc@users.noreply.github.com>
2022-02-21 12:20:36 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    $(".card-img-overlay").css({"border": "5px solid green"});
+    $(".card-title").css({"text-shadow": " 0 0 2px black, 0 0 2px black, 0 0 2px black, 0 0 2px black", "color": "yellow", "font-size": "200%"});
+    $(".card-text").css({"text-shadow": " 0 0 2px black, 0 0 2px black, 0 0 2px black, 0 0 2px black"});
+
+    
+    $("#banner").css({"font-size": "300%"});
+
+    $('#commentHelpInline').hide();
+    $('#userHelpInline').hide();
+    $('#titleHelpInline').hide();
+
+        
+    
+    $( ".card-img-overlay" ).hover(
+        function() {
+          $( this ).append( $( "<span> ***</span>" ) );
+        // }, function() {
+        //   $( this ).find( "span" ).last().remove();
+        }
+      ); 
+
+    function buttonEnable(){
+        let user = $('#inputUser').val();
+        let title = $('#inputTitle').val();
+        let comment = $('#inputComment').val();
+        if(user.length >= 6 && comment !== '' && title !=='') {
+            $('#submitBtn').prop('disabled', false);
+        }
+    }
+
+    let user = $('#inputUser').val();
+    let title = $('#inputTitle').val();
+    let comment = $('#inputComment').val();
+
+
+    $('#inputUser').focusin(function(){
+        buttonEnable();
+    $('#userHelpInline').show();
+         });
+
+    $('#inputUser').focusout(function(){
+        buttonEnable();
+        let user = $('#inputUser').val();
+        if (user.length >= 6){
+    $('#userHelpInline').hide();
+        }else { $('#userHelpInline').show();}
+        });
+
+        $('#inputUser').focusin(function(){
+            buttonEnable();
+            let user = $('#inputUser').val();
+            if (user.length >= 6){
+        $('#userHelpInline').hide();
+            }else { $('#userHelpInline').show();}
+            });
+
+    $('#inputTitle').focusin(function(){
+    $('#titleHelpInline').show();
+        });
+    
+    $('#inputTitle').focusout(function(){
+        buttonEnable();
+        if ($('#inputTitle').val() === ''){
+    
+    $('#titleHelpInline').show();
+        }else { $('#titleHelpInline').hide();}
+        });    
+
+        $('#inputComment').focusin(function(){
+            $('#commentHelpInline').show();
+                 });
+        
+            $('#inputComment').focusout(function(){
+                if ($('#inputComment').val() === ''){
+            $('#commentHelpInline').show();
+                }else { $('#commentHelpInline').hide();}
+                });    
+    
+    $('#inputComment').change(function(){
+        buttonEnable();
+
+});
+
+    // $('#inputUser').change(function(){
+    //     alert("The text has been changed.");
+    //         $('#submitBtn').prop('disabled', false);
+    //     });
+
+    
+
+    });
