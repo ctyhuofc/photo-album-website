2d579ff6754f8324d8cfe8d1899788145a982623
ctyhuofc <98438791+ctyhuofc@users.noreply.github.com>
2022-02-22 09:07:32 -0700

    Final

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ $(document).ready(function(){
     $( ".card-img-overlay" ).hover(
         function() {
         //   $(".card-title").css({'visibility': 'visible'});
-          $( this).children(".card-text").not(".banner").css({'visibility': 'visible'});
+          $(this).children(".card-text").not(".banner").css({'visibility': 'visible'});
 
             console.log("hover");
-            $( ".card-img-overlay").children(".card-text").css({'visibility': 'visible'});},
+            $( this).children(".card-text").css({'visibility': 'visible'});},
             function() {
-                $(this).not(".banner").css({'visibility': 'hidden'});
+                $(".card-text").not(".banner").css({'visibility': 'hidden'});
                 console.log("hover out");
                 $( this).children(".card-text").not(".banner").css({'visibility': 'hidden'});
               }
